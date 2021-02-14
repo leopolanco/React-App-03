876f03942eda0388ca16ad3c2dbdf9a70c10850c
leo polanco <leo.polanco.n@gmail.com>
2021-02-14 19:40:13 -0400

    2.3 Listo?

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,72 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import nba from '../api/nba'
+import {SearchIcon} from '../resources/svg'
 const Filter = () => {
-    return (
-        <div>
-            Filter
-        </div>
-    )
+  //this can be done better with a selector
+  //but this works
+  //this doesn't work.
+  const [searchNombre, setSearchNombre] = useState('')
+  const [searchApellido, setSearchApellido] = useState('')
+  const [result, setResult] = useState({})
+
+  const apiLastName = async lastname => {
+    try {
+      const response = await nba.get(`/players/firstName/${lastname}`)
+      setResult(response.data)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+  const apiFirstName = async firstname => {
+    try {
+      const response = await nba.get(`/players/firstName/${firstname}`)
+      setResult(response.data)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+  const handleSearch = e => {
+    if (!e.target.value || e.target.value.match(/[a-zA-Z]+/)) {
+      if (e.target.name === 'nombre') {
+        setSearchNombre(e.target.value)
+      } else if (e.target.name === 'apellido') {
+        setSearchApellido(e.target.value)
+      }
+    }
+  }
+  // useEffect(() => {
+  //   if (searchNombre.length>0) {
+  //     apiFirstName(searchNombre)
+  //   }
+  // }, [searchNombre])
+
+  // useEffect(() => {
+  //   if (searchApellido.length>0) {
+  //     apiLastName(searchApellido)
+  //   }
+  // }, [searchApellido])
+  return (
+    <div className='filter'>
+      <span><SearchIcon/></span>
+      <input
+        type='text'
+        placeholder='Nombre'
+        name='nombre'
+        value={searchNombre}
+        onChange={handleSearch}
+        onFocus={()=>setSearchNombre('')}
+      //clean the field when focused
+      />
+      <input
+        type='text'
+        placeholder='Apellido'
+        name='apellido'
+        value={searchApellido}
+        onChange={handleSearch}
+        onFocus={()=>setSearchApellido('')}
+      />
+    </div>
+  )
 }
 
 export default Filter
--- src/resources/svg.js
+++ src/resources/svg.js
@@ -1,155 +1,153 @@
 export const SwitchIcons = () => (
-    <svg
-  width="20"
-  height="20"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#fff"
->
-  <path
-    d="M7.72 21.78a.75.75 0 001.06-1.06L5.56 17.5h14.69a.75.75 0 000-1.5H5.56l3.22-3.22a.75.75 0 10-1.06-1.06l-4.5 4.5a.75.75 0 000 1.06l4.5 4.5zm8.56-9.5a.75.75 0 11-1.06-1.06L18.44 8H3.75a.75.75 0 010-1.5h14.69l-3.22-3.22a.75.75 0 011.06-1.06l4.5 4.5a.75.75 0 010 1.06l-4.5 4.5z"
-  />
-</svg>
+  <svg
+    width='20'
+    height='20'
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#fff'
+  >
+    <path d='M7.72 21.78a.75.75 0 001.06-1.06L5.56 17.5h14.69a.75.75 0 000-1.5H5.56l3.22-3.22a.75.75 0 10-1.06-1.06l-4.5 4.5a.75.75 0 000 1.06l4.5 4.5zm8.56-9.5a.75.75 0 11-1.06-1.06L18.44 8H3.75a.75.75 0 010-1.5h14.69l-3.22-3.22a.75.75 0 011.06-1.06l4.5 4.5a.75.75 0 010 1.06l-4.5 4.5z' />
+  </svg>
 )
 
 export const SquareIcon = () => (
   <svg
-  width="20"
-  height="20"
-  fill="currentColor"
-  color="#fff"
-  viewBox="0 0 32 32"
->
-  <g fillRule="evenodd">
-    <g id="Artboard-5" fillRule="nonzero">
-      <path d="M6 18h12V6H6v12zM4 4h16v16H4V4z" />
+    width='20'
+    height='20'
+    fill='currentColor'
+    color='#fff'
+    viewBox='0 0 32 32'
+  >
+    <g fillRule='evenodd'>
+      <g id='Artboard-5' fillRule='nonzero'>
+        <path d='M6 18h12V6H6v12zM4 4h16v16H4V4z' />
+      </g>
     </g>
-  </g>
-</svg>
+  </svg>
 )
 
 export const HambIcon = () => (
   <svg
-  width="24"
-  height="24"
-
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="currentColor"
-  color="#fff"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M4 6h16M4 12h16M4 18h16"
-  />
-</svg>
-) 
+    width='24'
+    height='24'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    color='#fff'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='{2}'
+      d='M4 6h16M4 12h16M4 18h16'
+    />
+  </svg>
+)
 
 export const StarIcon = () => (
   <svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#FFD700"
->
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path
-    d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-  />
-</svg>
-
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#FFD700'
+  >
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z' />
+  </svg>
 )
 
 export const CircleIcon = () => (
   <svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#000"
->
-  <path
-    fillRule="evenodd"
-    d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"
-  />
-</svg>
-
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#000'
+  >
+    <path
+      fillRule='evenodd'
+      d='M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z'
+    />
+  </svg>
 )
 
 export const DropArrow = () => (
   <svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#000"
->
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path d="M7 10l5 5 5-5z" />
-</svg>
-
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#000'
+  >
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M7 10l5 5 5-5z' />
+  </svg>
 )
 
 export const RedCircle = () => (
   <svg
-  width="20"
-  height="20"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#c90000"
->
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path
-    d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2z"
-  />
-</svg>
+    width='20'
+    height='20'
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#c90000'
+  >
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2z' />
+  </svg>
 )
 export const GreenCircle = () => (
   <svg
-  width="20"
-  height="20"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#004b00"
->
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path
-    d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2z"
-  />
-</svg>
+    width='20'
+    height='20'
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#004b00'
+  >
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2z' />
+  </svg>
 )
 export const BlueCircle = () => (
   <svg
-  width="20"
-  height="20"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#000080"
->
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path
-    d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2z"
-  />
-</svg>
+    width='20'
+    height='20'
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#000080'
+  >
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2z' />
+  </svg>
 )
 
 export const SidewayArrow = () => (
   <svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  color="#000"
->
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />
-</svg>
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#000'
+  >
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z' />
+  </svg>
+)
+export const SearchIcon = () => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    color='#000'
+  >
+    <path d='M0 0h24v24H0z' fill='none' />
+    <path d='M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' />
+  </svg>
 )
