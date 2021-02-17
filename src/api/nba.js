96ca85636f8f391bd9521fafb781f5389d32b7b3
leo polanco <leo.polanco.n@gmail.com>
2021-02-17 14:36:49 -0400

    Ready for deploy

--- src/api/nba.js
+++ src/api/nba.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
-const NBA_API_KEY = process.env.NBA_API_KEY
-//this variable changes in prod
-
 export default axios.create({
   baseURL: 'https://free-nba.p.rapidapi.com',
   headers: {
-    'x-rapidapi-key': NBA_API_KEY
+    'x-rapidapi-key': process.env.REACT_APP_NBA_API_KEY
   }
 })
